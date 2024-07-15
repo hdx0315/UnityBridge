1c2ed3494c81f2b51f570de0be5233a03837d1ba
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-07-16 01:59:29 +0530

    header menu routing fixed

---
+++ app/settings/search.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const search = () => {
+  return (
+    <View>
+      <Text>search</Text>
+    </View>
+  )
+}
+
+export default search
--- components/HomeHeader.jsx
+++ components/HomeHeader.jsx
@@ -2,27 +2,50 @@ import { View, Text, Image } from 'react-native';
 import React from 'react';
 import { icons } from '../constants';
 import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
+import { router } from 'expo-router';
 
 const HomeHeader = () => {
   return (
-    <View className="flex-row bg-bprimary justify-between items-center border-b-2 border-red-500 min-h-[40px]">
+    <View className="flex-row bg-bprimary justify-between items-center border-b-2 border-green-500 min-h-[40px]">
       <View>
         <Text className="pl-4 pt-4 pb-4 text-xl font-pbold text-white">Chats</Text>
       </View>
       <View>
         <Menu>
           <MenuTrigger>
-            <Image source={icons.menu} className="w-6 h-6 ml-auto mr-1" resizeMode="contain" />
+            <Image 
+                source={icons.menu} 
+                className="w-6 h-6 ml-auto mr-1" 
+                resizeMode="contain" 
+            />
           </MenuTrigger>
+
           <MenuOptions>
-            <MenuOption onSelect={() => alert('Profile')} className="bg-green-700">
-              <Text className="text-white text-lg">Profile</Text>
+            <MenuOption 
+                onSelect={() => router.push('settings/profile')} 
+                className="bg-green-700"
+            >
+                <Text className="text-white text-lg">
+                    Profile
+                </Text>
             </MenuOption>
-            <MenuOption onSelect={() => alert('Settings')} className="bg-green-700">
-              <Text className="text-white text-lg">Settings</Text>
+
+            <MenuOption 
+                onSelect={() => router.push('settings/settings')} 
+                className="bg-green-700"
+            >
+                <Text className="text-white text-lg">
+                    Settings
+                </Text>
             </MenuOption>
-            <MenuOption onSelect={() => alert('Logout')} className="bg-green-700">
-              <Text className="text-white text-lg">Logout</Text>
+
+            <MenuOption 
+                onSelect={() => router.push('settings/search')} 
+                className="bg-green-700"
+            >
+                <Text className="text-white text-lg">
+                    Search
+                </Text>
             </MenuOption>
           </MenuOptions>
         </Menu>
