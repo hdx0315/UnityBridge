a88a99284147cd97518fab4a881eedf32f5ed223
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-09-24 06:26:07 +0530

    cyan test

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -6,7 +6,7 @@ const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoadin
     <TouchableOpacity 
         onPress={handlePress}
         activeOpacity={0.7}
-        className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading? 'opacity-50': ''}`}
+        className={`bg-cyan-500 rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading? 'opacity-50': ''}`}
         disabled={isLoading}    
     >
         
