d6e9f73556cc855df90b611fe69247be357ba768
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-11-11 16:44:07 +0530

    Update userStore.js

--- stores/userStore.js
+++ stores/userStore.js
@@ -1,16 +1,26 @@
+//userStore.js 
 
+import { create } from 'zustand';
 
-import {create} from 'zustand';
-
+// Store for user profile information
 export const useUserStore = create((set) => ({
   name: '',
   profilePic: null,
   setNameAndProfilePic: (name, profilePic) => set({ name, profilePic }),
 }));
 
+// Store for call information
 export const useUserCallStore = create((set) => ({
   name: '',
   profilePic: null,
   calltype: '', // missed, outgoing, incoming
   setNameProfilePicAndCalltype: (name, profilePic, calltype) => set({ name, profilePic, calltype }),
+}));
+
+// Store for managing users and chat data
+export const useChatStore = create((set) => ({
+  users: [],
+  chats: [],
+  setUsers: (users) => set({ users }),
+  setChats: (chats) => set({ chats }),
 }));
