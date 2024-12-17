ab91ce873f098ede9838bd37461976eaed451492
Thandu-darshana <tharindurathnasuriya@gmail.com>
2024-12-17 11:56:43 +0530

    add test ID attributes

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -6,6 +6,7 @@ const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoadin
     <TouchableOpacity 
         onPress={handlePress}
         activeOpacity={0.7}
+        testID="custom-button"
         className={`bg-bprimary-700 border-2 border-bprimary-700 rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading? 'opacity-50': ''}`}
         disabled={isLoading}    
     >
